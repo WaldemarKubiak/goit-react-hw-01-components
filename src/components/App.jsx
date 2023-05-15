6cfda66bad7eb39f4ce3edf2f126e442c6648885
Waldemar Kubiak <kubiak.waldemar@gmail.com>
2023-05-15 20:19:28 +0200

    Statistics Task first version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
+import React from 'react';
 import { Profile } from './Profile/Profile';
 import user from './Profile/user';
+import { Statistics } from './Statistics/Statistics';
+import data from './Statistics/data';
 
 export const App = () => {
   return (
@@ -11,6 +14,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,31 +1,33 @@
 import PropTypes from 'prop-types';
 import styles from './Profile.module.css';
 
-export const Profile = ({ username, tag, location, avatar, stats }) => (
-  <div className={styles.profile}>
-    <div className={styles.description}>
-      <img src={avatar} alt="User avatar" className={styles.avatar} />
-      <p className={styles.profileName}>{username}</p>
-      <p className={styles.profileTag}>@{tag}</p>
-      <p className={styles.profileLocation}>{location}</p>
-    </div>
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt="User avatar" className={styles.avatar} />
+        <p className={styles.profileName}>{username}</p>
+        <p className={styles.profileTag}>@{tag}</p>
+        <p className={styles.profileLocation}>{location}</p>
+      </div>
 
-    <ul className={styles.stats}>
-      <li className={styles.followersList}>
-        <span className={styles.label}>Followers</span>
-        <span className={styles.quantity}>{stats.followers}</span>
-      </li>
-      <li className={styles.viewsList}>
-        <span className={styles.label}>Views</span>
-        <span className={styles.quantity}>{stats.views}</span>
-      </li>
-      <li className={styles.likesList}>
-        <span className={styles.label}>Likes</span>
-        <span className={styles.quantity}>{stats.likes}</span>
-      </li>
-    </ul>
-  </div>
-);
+      <ul className={styles.stats}>
+        <li className={styles.followersList}>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>{stats.followers}</span>
+        </li>
+        <li className={styles.viewsList}>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>{stats.views}</span>
+        </li>
+        <li className={styles.likesList}>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 Profile.propTypes = {
   username: PropTypes.string.isRequired,
